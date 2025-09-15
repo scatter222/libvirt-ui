17b7e4f2a0cefd1450e06d7722a547520166e7d9
rhys <rhys-gordon@live.com.au>
2025-09-16 09:45:55 +1000

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,30 @@
+import { ThemeProvider } from '@/app/components/theme-provider';
+import Titlebar from '@/app/components/titlebar';
+import { useRendererListener } from '@/app/hooks';
+import { LandingScreen } from '@/app/screens/landing';
+import { MenuChannels } from '@/channels/menuChannels';
+
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
+
+const onMenuEvent = (_: Electron.IpcRendererEvent, channel: string, ...args: unknown[]) => {
+  electron.ipcRenderer.invoke(channel, args);
+};
+
+export default function App () {
+  useRendererListener(MenuChannels.MENU_EVENT, onMenuEvent);
+
+  return (
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <Router>
+        <div className='flex flex-col h-full'>
+          <Titlebar />
+          <main className='flex-1 overflow-auto'>
+            <Routes>
+              <Route path='/' Component={LandingScreen} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
